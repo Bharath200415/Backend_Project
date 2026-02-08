cad50d4eac8d5d4f599e3f141ad5d9fa20c7aafb
Bharath200415 <bharath200415@gmail.com>
2026-02-08 16:56:00 +0530

    utils

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    credentials:true
+}));
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export {app}
